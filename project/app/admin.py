899c82a8cf813743485b9e73e2b12f50a77c1238
Vishnu Suresh <vish3eyedman.all@gmail.com>
2026-03-09 16:53:42 +0530

    Initialize app and register basic models in admin

---
+++ project/app/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(Login)
+admin.site.register(User)
+admin.site.register(Production_Staff)
+admin.site.register(Attendance)
+admin.site.register(Loan)
+admin.site.register(Payment)
+admin.site.register(Product)
+admin.site.register(Booking)
